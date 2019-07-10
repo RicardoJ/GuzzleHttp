8a0c738f0d091fe5e834efb459f2a6cdc7bb2242
RicardoJ <rjdiazzh@gmail.com>
2019-07-10 17:38:34 -0500

    refactorizando codigo en clases de repositories y desacoplando

--- app/Repositories/Planning.php
+++ app/Repositories/Planning.php
@@ -3,15 +3,13 @@
 namespace App\Repositories;
 use GuzzleHttp\Client;
 class Planning{
+    protected $client;
+    public function __construct(Client $client){
+        $this->client =  $client;
+    }
+
     public function listAll($id){
-        $client = new Client([
-                
-            'base_uri' => 'http://localhost:5001/api/v1/',
-           
-            'timeout'  => 2.0,
-        ]);
-            $response = $client->request('GET', "projects/{$id}/integration/planning");
-        
+            $response = $this->client->request('GET', "projects/{$id}/integration/planning");
             return json_decode( $response->getBody()->getContents() );
     }
 }
--- app/Repositories/Projects.php
+++ app/Repositories/Projects.php
@@ -3,16 +3,13 @@
 namespace App\Repositories;
 use GuzzleHttp\Client;
 class Projects{
-     public function listAll(){
 
-        $client = new Client([
-                
-            'base_uri' => 'http://localhost:5001/api/v1/',
-         
-            'timeout'  => 2.0,
-        ]);
-            $response = $client->request('GET', 'projects');
-        
+protected $client;
+public function __construct(Client $client ){
+    $this->client =  $client;
+}
+     public function listAll(){
+            $response = $this->client->request('GET', 'projects');
             return json_decode( $response->getBody()->getContents() );
      }
     
